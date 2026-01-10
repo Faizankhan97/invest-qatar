5f173d2c8543ca493480e7bc7fd0659c2a560bc3
Faizan Khan <faizikhan753@gmail.com>
2026-01-10 22:55:21 +0500

    invest qatar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,13 +8,13 @@ import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid p-0">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid p-0">
+        <a className="navbar-brand" href="#">
           <img src={Logo} alt="" />
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -22,42 +22,42 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
                 Why Qatar <img src={Arrow} alt="" />
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Sectors & Opportunities <img src={Arrow} alt="" />
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-disabled="true">
+            <li className="nav-item">
+              <a className="nav-link" aria-disabled="true">
                 Incentives <img src={Arrow} alt="" />
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-disabled="true">
+            <li className="nav-item">
+              <a className="nav-link" aria-disabled="true">
                 How we help <img src={Arrow} alt="" />
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-disabled="true">
+            <li className="nav-item">
+              <a className="nav-link" aria-disabled="true">
                 Media Centre <img src={Arrow} alt="" />
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-disabled="true">
+            <li className="nav-item">
+              <a className="nav-link" aria-disabled="true">
                 Resources <img src={Arrow} alt="" />
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-disabled="true">
+            <li className="nav-item">
+              <a className="nav-link" aria-disabled="true">
                 About us <img src={Arrow} alt="" />
               </a>
             </li>
@@ -66,17 +66,17 @@ const Navbar = () => {
             <img className="country" src={Country} alt="" />
             <img className="person" src={PersonArmsSpread} alt="" />
             <span></span>
-            <form class="d-flex" role="search">
+            <form className="d-flex" role="search">
               <div className="input-search">
                 <img src={Search} alt="" />
                 <input
-                  class="form-control"
+                  className="form-control"
                   type="search"
                   placeholder="Search..."
                   aria-label="Search"
                 />
               </div>
-              <button class="btn" type="submit">
+              <button className="btn" type="submit">
                 Create an Account
               </button>
             </form>
